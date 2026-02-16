e9abab3c03020fe1b8fdf5f18056596b80c632e6
Yangji <yangjuhyolmo13@gamil.com>
2026-02-17 00:14:56 +0545

    add localstorage

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let cells = document.querySelectorAll(".cell");
 let statusText = document.getElementById("status");
 let restartBtn = document.getElementById("restart-button")
+let playerOInput = document.getElementById("playerO")
+let playerXInput = document.getElementById("playerX")
+let historyList = document.getElementById("history")
+let saveBtn = document.getElementById("savePlayers")
 
+let players = { X: "Player X", O: "Player O" };
 let current = "X";
 let board = ["", "", "", "", "", "", "", "", ""];
 let running = false;
@@ -11,14 +16,37 @@ let wins = [
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ];
-initializeGame()
+
+initializeGame();
+loadhistory();
+
 
 function initializeGame() {
     cells.forEach(cell => cell.addEventListener('click', cellClicked))
     restartBtn.addEventListener("click", restart)
+    saveBtn.addEventListener("click", savePlayers);
     statusText.textContent = "Player " + current + "'s turn";
     running = true;
 }
+
+function savePlayers() {
+    players.X = playerXInput.value || "Player X";
+    players.O = playerOInput.value || "Player O";
+
+    localStorage.setItem("players", JSON.stringify(players));
+    statusText.textContent = "Player " + current + "'s turn";
+}
+
+function loadPlayers() {
+    const savedPlayers = localStorage.getItem("players");
+
+    if (savedPlayers) {
+        players = JSON.parse(savedPlayers);
+        playerXInput.value = players.X;
+        playerOInput.value = players.O;
+    }
+}
+
 
 function cellClicked() {
     const cellIndex = this.getAttribute("cellIndex")
@@ -82,7 +110,7 @@ function restart() {
     for (let i = 0; i < cells.length; i++) {
         cells[i].textContent = "";
         board[i] = "";
-        running= false;
+        running = false;
     }
     current = "X";
     statusText.textContent = "Player X's turn";
